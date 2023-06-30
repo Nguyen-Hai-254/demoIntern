fa1cfd4ac71c28039fbb7608e4821a75ea92063f
Nguyen-Hai-254 <phamnguyenhai0123@gmail.com>
2023-06-30 11:37:53 +0700

    setup enviroment and config

---
+++ src/config/connectDB.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.database, 'root', null, {
+    host: process.env.HOST,
+    dialect: 'mysql',
+    port: process.env.PORT_DB || 3307,
+    logging: false
+});
+
+let connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = connectDB;
---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import initWebRoutes from "./route/web"
+import connectDB from "./config/connectDB"
+import cors from "cors"
+require('dotenv').config();
+
+
+
+
+let app = express();
+app.use(cors({ credentials: true, origin: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+connectDB();
+
+initWebRoutes(app);
+
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('BackEnd NodeJS is running on the port: ', port);
+})
