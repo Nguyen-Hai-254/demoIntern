709623c4c5d4da0926088a90354e8b3b69aaad61
Nguyen-Hai-254 <phamnguyenhai0123@gmail.com>
2023-07-02 16:37:38 +0700

    edit file seed user, create seed post

--- src/seeders/20230701073156-user.js
+++ src/seeders/20230701073156-user.js
@@ -2,25 +2,32 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+    async up(queryInterface, Sequelize) {
 
-    await queryInterface.bulkInsert('Users', [{
-      email: 'hai@gmail.com',
-      password: 'hai',
-      firstName: 'Hai',
-      lastName: 'Nguyen',
-      address: 'KTX khu A',
-      phoneNumber: '0147258369'
-    }], {});
+        await queryInterface.bulkInsert('Users', [{
+            email: 'dat@gmail.com',
+            password: 'dat',
+            firstName: 'Công Đạt',
+            lastName: 'Hoàng',
+            address: 'Tân Bình, TP.HCM',
+            phoneNumber: '0147258369'
+        }, {
+            email: 'nguyen@gmail.com',
+            password: 'nguyen',
+            firstName: 'Nguyen Hoang',
+            lastName: 'Pham',
+            address: 'Quận 7, TP. HCM',
+            phoneNumber: '0147258369'
+        }]);
 
-  },
+    },
 
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
 };
